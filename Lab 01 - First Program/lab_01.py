807264100354f733c6e4889cbbf5c2d8bf853ee4
Raely W <whitesr@hartwick.edu>
2025-02-07 07:59:13 -0500

    Testing

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,8 @@
-Soup
+print("meow meow meow meow")
+print("air \"quotes")
+print( "\"Nothing is impossible. The word itself says 'I'm Possible!'.\"")
+print("""meow
+    meow
+        meow
+      meow
+        meow.""")
